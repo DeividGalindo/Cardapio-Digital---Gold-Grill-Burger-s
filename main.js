5fad8667b622403b77e4e199318870cc630b20e7
Deivid Galindo <deivid.dd223@gmail.com>
2025-03-14 20:29:18 -0300

    Criado as variaveis

---
+++ main.js
@@ -0,0 +1,6 @@
+// Simulação de adição de produtos à cesta do site
+
+const botoesAdicionar = document.querySelectorAll(".adicionar");
+const listaPedido     = document.getElementById("lista-pedido");
+const totalValores    = document.getElementById("total");
+let total = 0;
